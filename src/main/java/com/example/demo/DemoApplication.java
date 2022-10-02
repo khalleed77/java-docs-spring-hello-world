e0a4a38b90c9fbe094c49c7cf7e2a4b18b52b32f
khalleed77 <114617689+khalleed77@users.noreply.github.com>
2022-10-02 23:55:26 +0300

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World!";
+		return "Hello from App Service on Azure Arc-enabled Kubernetes cluster";
 	}
 }
